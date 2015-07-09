6ca3023dc1f14f63143692b8155e67a83f9cc11c
Taurvi <srimbakusumo@gmail.com>
2015-07-08 22:45:21 -0700

    Added form validation. ISSUE: When entering in data and then deleting it, it validates form as valid.

--- js/script.js
+++ js/script.js
@@ -9,10 +9,25 @@ var debugMsg = function(msg) {
 if (debugMode)
     debugMsg('Debug Mode Active!')
 
+// Global Vars
+var userName;
+var gameActive;
+
 // Create Angular App
 var ngApp = angular.module('spotifyApp', []);
 
 // Creates Controller
-ngApp.controller('changeSearchImg', function($scope) {
-    $scope.searchType = 'search-artist';
-});
+ngApp.controller('primary', ['$scope', function($scope) {
+    $scope.userName = false;
+    $scope.gameActive = false;
+
+    // When game start is clicked
+    $scope.startGame = function() {
+        $scope.gameActive = true;
+        $scope.userName = $scope.inputName;
+    };
+
+    $scope.checkUserValid = function() {
+        return ($scope.formSetup.username.$error.minlength || ($scope.formSetup.username.$touched && !$scope.formSetup.username.$dirty));
+    }
+}]);
