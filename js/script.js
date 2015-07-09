9e62356e8cb441e41c5e732d32530e9946a86ce0
Taurvi <srimbakusumo@gmail.com>
2015-07-09 15:20:52 -0700

    Imported angular-spotify.js

--- js/script.js
+++ js/script.js
@@ -12,12 +12,22 @@ if (debugMode)
 // Global Vars
 
 // Create Angular App
-var ngApp = angular.module('spotifyApp', []);
+var ngApp = angular.module('spotifyApp', ['spotify']);
 
 // Creates Controller
 ngApp.controller('primary', ['$scope', '$http', function($scope, $http) {
+    app.config(function (SpotifyProvider) {
+        SpotifyProvider.setClientId('e9a7c84eb4d1413ea673caabdf6c4106');
+        SpotifyProvider.setRedirectUri('<CALLBACK_URI>');
+        SpotifyProvider.setScope('<SCOPE>');
+        // If you already have an auth token
+        SpotifyProvider.setAuthToken('<AUTH_TOKEN>');
+    });
+
     $scope.userName = false;
     $scope.gameActive = false;
+    $scope.audioObject = {};
+    $scope.featuredUrl = 'https://api.spotify.com/v1/browse/featured-playlists';
 
     // When game start is clicked
     $scope.startGame = function() {
